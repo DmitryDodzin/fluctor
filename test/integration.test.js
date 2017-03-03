5aba8094adb760195fad105bc8de13b2be94a7ee
Dmitry Dodzin <d.dodzin@gmail.com>
2017-03-03 13:01:22 +0200

    Adding Intergation Tests

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,5 +1,7 @@
 
+const sinon = require('sinon');
 const chai = require('chai');
+const assert = chai.assert;
 const expect = chai.expect;
 
 const Redis = require('ioredis');
@@ -21,13 +23,42 @@ describe('Fluctor', () => {
 
         fluctor = new Fluctor({ initial: initial_state });
         fluctor.on('ready', () => done()); 
-        fluctor.on('error', err => console.log(err));
       });
     });
   });
 
   it('State Loaded', () => {
     expect(fluctor.state).to.deep.equal(initial_state);
+  });
+
+  it('Throw Err on error', ()=>{
+
+    let callback = sinon.spy();
+
+    fluctor.on('error', callback);
+    fluctor.sync_client.emit('error');
+
+    assert(callback.calledOnce);
+  });
+
+  it('Tran and Transaction', () => {
+
+    let transaction_interface = fluctor.tran;
+
+    sinon.stub(fluctor.sync_client, 'publish');
+
+    try{
+
+      let transaction = transaction_interface.begin();
+
+      transaction.commit();
+
+      assert(fluctor.sync_client.publish.calledWith(transaction));
+
+    } finally {
+      fluctor.sync_client.publish.restore();      
+    }
+
   });
 
 });
